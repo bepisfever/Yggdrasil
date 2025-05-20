1d96ae72c57581507fc222829e7bbdb6ab07b532
Richard <richardbailey@outlook.com>
2025-05-20 15:22:28 +0100

    Reposition the xp bar to be inside the left ui panel

--- code/level.lua
+++ code/level.lua
@@ -40,42 +40,6 @@ function Game:start_run(args) --To add level meter.
 
     if G and G.jokers then
         G.GAME.ygg_current_xp = 0
-        if G.ygg_xp_bar then G.ygg_xp_bar:remove() end
-        local setPadding = 0.02
-        local ygg_xp_bar_text = {n = G.UIT.O, config = {object = DynaText({scale = 0.5, string = {{ref_table = ygg_to_refer_level_bar, ref_value = "text"}}, maxw = 9, colours = { G.C.WHITE }, float = true, shadow = true, silent = true})}}
-    
-        G.ygg_xp_bar = UIBox {
-            definition = {
-                n = G.UIT.ROOT,
-                config = {
-                    align = "cm",
-                    padding = setPadding,
-                    colour = G.C.CLEAR,
-                },
-                nodes = {
-                    {
-                        n = G.UIT.C,
-                        config = {align = "cr", padding = 0.05},
-                        nodes = {
-                            {n = G.UIT.R, config = {align = "cm", padding = setPadding}, nodes = {
-                                create_progress_bar({reverse_fill = false, bar_rotation = "Horizontal", w = 10, h = 0.25, ref_table = G.GAME, ref_value = 'ygg_current_xp', min = 0, max = 100})
-                            }},
-                            {n = G.UIT.R, config = {align = "tm", padding = setPadding}, nodes = {
-                                ygg_xp_bar_text
-                            }},
-                        }
-                    },
-                }
-            },
-            config = {
-                align = "tr",
-                offset = { x = -9, y = 3.85},
-                major = G.jokers,
-                bond = 'Strong'
-            }
-        }
-        G.ygg_xp_bar.states.hover.can = false
-        G.ygg_xp_bar.states.click.can = false
     end
 
 end

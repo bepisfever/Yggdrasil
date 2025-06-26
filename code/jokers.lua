cfb12ea6c3416a9f549c6e79d63a5b6713096f86
BepisFever <nguyendoangiabao220808@gmail.com>
2025-06-26 15:38:30 +0700

    Update jokers.lua

--- code/jokers.lua
+++ code/jokers.lua
@@ -27,6 +27,7 @@ SMODS.Joker {
    in_pool = function(self, args)
       return false
    end,
+   config = {extra = {gains = {mult = 0, chips = 0, xmult = 1, xchips = 1, multMulti = 1, chipsMulti = 1}, last_gains = {mult = 0, chips = 0, xmult = 1, xchips = 1, multMulti = 1, chipsMulti = 1}}},
 
    update = function(self,card,dt)
       if G and G.GAME then
@@ -60,7 +61,7 @@ SMODS.Joker {
          end
    
          if if_skill_obtained("ygg_mult10") then
-            gains["hand_size"] = gains["hand_size"] + math.floor((card.ability.current_mult or 0)/100)
+            gains["hand_size"] = gains["hand_size"] + math.floor((card.ability.extra.last_gains.mult or 0)/100)
          end
 
          if if_skill_obtained("ygg_MoreFluff_2") and next(SMODS.find_mod("MoreFluff")) then
@@ -112,15 +113,14 @@ SMODS.Joker {
    end,
 
    calculate = function(self,card,context)
-      local gains = {
-         mult = 0, chips = 0, xmult = 1, xchips = 1, multMulti = 1, chipsMulti = 1
-      }
+      local gains = card.ability.extra.gains
 
       if card.edition then
          card:set_edition(nil,true)
       end
 
       local function check_skill_gains(i,v)
+         gains = {mult = 0, chips = 0, xmult = 1, xchips = 1, multMulti = 1, chipsMulti = 1}
          local info = get_skill_info(i) or {}
          for i2,_ in pairs(gains) do
             local multi = 1
@@ -131,136 +131,132 @@ SMODS.Joker {
          end
       end
 
-      for i,v in pairs(G.PROFILES[G.SETTINGS.profile].skill_perks or {}) do
-         if not DisabledSkills[i] then
-            check_skill_gains(i,v)
-         end
-      end
-
-      for i,v in pairs(G.GAME.skill_perks or {}) do
-         if not DisabledSkills[i] then
-            check_skill_gains(i,v)
-         end
-      end
-
-      for i,v in pairs(gains) do
-         if card.ability[i] then
-            gains[i] = gains[i] + card.ability[i]
-         end
-      end
-
-      if if_skill_obtained("ygg_mult3") then
-         gains["xmult"] = gains["xmult"] + (get_skill_info("ygg_mult3")["config"]["xmult"] * math.floor(gains["mult"]/10))
-      end
-
-      if if_skill_obtained("ygg_mult8") then
-         for _,v in ipairs(G.playing_cards or {}) do
-            if v:is_face() then
-               gains["mult"] = gains["mult"] + 5
+      local function load_gains()
+         for i,v in pairs(G.PROFILES[G.SETTINGS.profile].skill_perks or {}) do
+            if not DisabledSkills[i] then
+               check_skill_gains(i,v)
             end
          end
-      end
 
-      if if_skill_obtained("ygg_mult12") then
-         if #G.jokers.cards < 19 and G.GAME.current_round.hands_played < 2 then
-            gains["mult"] = gains["mult"] + 87
-         end
-      end
-
-      if if_skill_obtained("ygg_chips5") then
-         gains["xchips"] = math.max(gains["xchips"],1.1)
-      end
-
-      if if_skill_obtained("ygg_chips6") then
-         local stop = false
-         repeat
-            if pseudorandom("ygg_chips6") <= 3/4 then
-               local multi = 1
-               gains["chips"] = gains["chips"] + (25 * multi) 
-            else
-               stop = true
+         for i,v in pairs(G.GAME.skill_perks or {}) do
+            if not DisabledSkills[i] then
+               check_skill_gains(i,v)
             end
-         until stop
-      end
-
-      if if_skill_obtained("ygg_chips7") then
-         gains["chips"] = gains["chips"] + (gains["mult"]/10)
-      end
-
-      if if_skill_obtained("ygg_chips8") then
-         if G.GAME.current_round.hands_left == 1 then
-            gains["xchips"] = gains["xchips"] + 0.5
          end
-      end
 
-      if if_skill_obtained("ygg_chips10") then
-         if pseudorandom("ygg_chips10") <= 1/2 then
-            gains["xchips"] = gains["xchips"] + 0.5
-         else
-            gains["chips"] = gains["chips"] - 50
+         for i,v in pairs(gains) do
+            if card.ability[i] then
+               gains[i] = gains[i] + card.ability[i]
+            end
          end
-      end
 
-      if if_skill_obtained("ygg_AKYRS_1") and AKYRS then
-         for _,v in ipairs(G.jokers.cards or {}) do
-            local key = v.config.center.key
-            local desc = (G.localization.descriptions.Joker[key] or {})["text"] or nil
-            
-            if desc then
-               local chars = 0
-               local is_multi_box = false
-               for i,v in pairs(desc) do
-                  if type(v) == "table" then is_multi_box = true end
+         if if_skill_obtained("ygg_mult3") then
+            gains["xmult"] = gains["xmult"] + (get_skill_info("ygg_mult3")["config"]["xmult"] * math.floor(gains["mult"]/10))
+         end
+
+         if if_skill_obtained("ygg_mult8") then
+            for _,v in ipairs(G.playing_cards or {}) do
+               if v:is_face() then
+                  gains["mult"] = gains["mult"] + 5
                end
+            end
+         end
 
-               if is_multi_box then
-                  for _,box in ipairs(desc) do
-                     for _,str in ipairs(box) do
+         if if_skill_obtained("ygg_mult12") then
+            if #G.jokers.cards < 19 and G.GAME.current_round.hands_played < 2 then
+               gains["mult"] = gains["mult"] + 87
+            end
+         end
+
+         if if_skill_obtained("ygg_chips5") then
+            gains["xchips"] = math.max(gains["xchips"],1.1)
+         end
+
+         if if_skill_obtained("ygg_chips6") then
+            local stop = false
+            repeat
+               if pseudorandom("ygg_chips6") <= 3/4 then
+                  local multi = 1
+                  gains["chips"] = gains["chips"] + (25 * multi) 
+               else
+                  stop = true
+               end
+            until stop
+         end
+
+         if if_skill_obtained("ygg_chips7") then
+            gains["chips"] = gains["chips"] + (gains["mult"]/10)
+         end
+
+         if if_skill_obtained("ygg_chips8") then
+            if G.GAME.current_round.hands_left == 1 then
+               gains["xchips"] = gains["xchips"] + 0.5
+            end
+         end
+
+         if if_skill_obtained("ygg_chips10") then
+            if pseudorandom("ygg_chips10") <= 1/2 then
+               gains["xchips"] = gains["xchips"] + 0.5
+            else
+               gains["chips"] = gains["chips"] - 50
+            end
+         end
+
+         if if_skill_obtained("ygg_AKYRS_1") and AKYRS then
+            for _,v in ipairs(G.jokers.cards or {}) do
+               local key = v.config.center.key
+               local desc = (G.localization.descriptions.Joker[key] or {})["text"] or nil
+               
+               if desc then
+                  local chars = 0
+                  local is_multi_box = false
+                  for i,v in pairs(desc) do
+                     if type(v) == "table" then is_multi_box = true end
+                  end
+
+                  if is_multi_box then
+                     for _,box in ipairs(desc) do
+                        for _,str in ipairs(box) do
+                           local normalizedStr = Yggdrasil.toNormalString(str)
+                           local final = string.gsub(normalizedStr," ","")
+                           final = string.gsub(final, "#", "")
+                           chars = chars + string.len(final)
+                        end
+                     end
+                  else
+                     for _,str in ipairs(desc) do
                         local normalizedStr = Yggdrasil.toNormalString(str)
                         local final = string.gsub(normalizedStr," ","")
                         final = string.gsub(final, "#", "")
                         chars = chars + string.len(final)
                      end
                   end
-               else
-                  for _,str in ipairs(desc) do
-                     local normalizedStr = Yggdrasil.toNormalString(str)
-                     local final = string.gsub(normalizedStr," ","")
-                     final = string.gsub(final, "#", "")
-                     chars = chars + string.len(final)
-                  end
+                  gains["chips"] = gains["chips"] + (0.5 * chars)
                end
-               gains["chips"] = gains["chips"] + (0.5 * chars)
             end
          end
-      end
 
-      if if_skill_obtained("ygg_AKYRS_4") and AKYRS then
-         gains["xmult"] = gains["xmult"] + (0.5 * math.max(40 - love.timer.getFPS(), 0))
-      end
+         if if_skill_obtained("ygg_AKYRS_4") and AKYRS then
+            gains["xmult"] = gains["xmult"] + (0.5 * math.max(40 - love.timer.getFPS(), 0))
+         end
 
-      gains["mult"] = gains["mult"] * math.max(gains["multMulti"], 0)
-      gains["chips"] = gains["chips"] * math.max(gains["chipsMulti"], 0)
+         gains["mult"] = gains["mult"] * math.max(gains["multMulti"], 0)
+         gains["chips"] = gains["chips"] * math.max(gains["chipsMulti"], 0)
+
+         card.ability.extra.last_gains = table.clone(card.ability.extra.gains)
+      end
 
       if not context.retrigger_joker and not context.blueprint then
          card.ability.current_mult = gains["mult"]
          if context.joker_main then
-            SMODS.calculate_context({mat_joker_main = true})
+            load_gains()
             if if_skill_obtained("ygg_mult7") then
                SMODS.calculate_effect({mult = gains["mult"]},G.deck)
             end
             SMODS.calculate_effect({mult = gains["mult"], chips = gains["chips"], xchips = gains["xchips"], xmult = gains["xmult"]},G.deck)
          end
 
-         if context.other_joker then
-            SMODS.calculate_context({mat_other_joker = context.other_joker})
-         end
-         if context.other_consumeable then
-            SMODS.calculate_context({mat_other_consumeable = context.other_consumeable})
-         end
-
          if context.end_of_round and context.main_eval then
-            SMODS.calculate_context({mat_end_of_round = true})
             if if_skill_obtained("ygg_spec1_upgrade") then
                local tarot_pool = {}
                for _,v in ipairs(G.consumeables.cards) do
@@ -287,7 +283,6 @@ SMODS.Joker {
          end
 
          if context.individual and context.cardarea == G.play then
-            SMODS.calculate_context({mat_individual = true, cardarea = G.play, other_card = context.other_card, scoring_hand = context.scoring_hand})
             if if_skill_obtained("ygg_AKYRS_2") and AKYRS then
                if context.other_card:get_letter_with_pretend() == "y" or context.other_card:get_letter_with_pretend() == "Y" then
                   return{
